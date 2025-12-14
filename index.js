1d669393ec14c8325de801544fd3af42e717ddc1
anindyamutsuddi4 <anindyamutsuddi4@gmail.com>
2025-12-14 16:02:33 +0600

    pushed

--- index.js
+++ index.js
@@ -4,9 +4,37 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./bloodlink-1676e-firebase-adminsdk-fbsvc-89d5065ae7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 //middleware
 app.use(express.json())
 app.use(cors())
+const verifytoken = async (req, res, next) => {
+  console.log(req.headers?.authorization)
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  try {
+    const idtoken = token.split(' ')[1]
+    const decoded = await admin.auth().verifyIdToken(idtoken)
+    console.log(decoded)
+    req.decoded_email = decoded.email//decoded er moddhe email r value ekhane set kore dicchi
+    //eta hocche jar token tar email nicchi
+
+    next()
+  } catch (err) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yj7cq3y.mongodb.net/?appName=Cluster0`;
 
@@ -25,6 +53,16 @@ async function run() {
     const db = client.db('BloodLink')
     const usercollection = db.collection('users')
     const requestcollection = db.collection('requests')
+
+    const verifyadmin = async (req, res, next) => {
+      const email = req.decoded_email//verifytoken middleware theke ei email ta pabo
+      const query = { email }
+      const user = await usercollection.findOne(query)
+      if (!user || user.role != "admin") {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      next()
+    }
     app.post('/users', async (req, res) => {
       const donor = req.body
       donor.role = "donor"
@@ -33,14 +71,17 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/users/:email/role', async (req, res) => {
+    app.get('/users/:email/role', verifytoken, async (req, res) => {
       const email = req.params.email
       const query = { email }
+      if (email !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
       const user = await usercollection.findOne(query)
       res.send({ role: user?.role || "user" })
       //user thakle role pathai diba noile 'user' pathaba
     })
-    app.get('/allusers', async (req, res) => {
+    app.get('/allusers', verifytoken,verifyadmin, async (req, res) => {
       const query = {}
       const { limit, skip, filter } = req.query
       if (filter && filter != "all") {
@@ -54,16 +95,22 @@ async function run() {
         totalCount
       })
     })
-    app.get('/users/:email', async (req, res) => {
+    app.get('/users/:email', verifytoken, async (req, res) => {
       const email = req.params.email;
       const query = { email }
+      if (email !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
       const result = await usercollection.findOne(query)
       res.send(result)
     })
 
-    app.patch('/users/:email', async (req, res) => {
+    app.patch('/users/:email', verifytoken, async (req, res) => {
       const email = req.params.email
       const query = { email }
+      if (email !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
       const fields = ["name", "divisions", "district", "bloodgroup"];
       const updateData = {};
       fields.forEach(f => {
@@ -74,7 +121,7 @@ async function run() {
       const result = await usercollection.updateOne(query, { $set: updateData })
       res.send(result)
     })
-    app.patch('/userstatus/:id', async (req, res) => {
+    app.patch('/userstatus/:id', verifytoken, async (req, res) => {
       const id = req.params.id
       const statusupdate = req.body
       const query = { _id: new ObjectId(id) }
@@ -87,7 +134,7 @@ async function run() {
       const result = await usercollection.updateOne(query, update)
       res.send(result)
     })
-    app.patch('/userrole/:id', async (req, res) => {
+    app.patch('/userrole/:id', verifytoken, async (req, res) => {
       const id = req.params.id
       const roleupdate = req.body
       const query = { _id: new ObjectId(id) }
@@ -99,16 +146,16 @@ async function run() {
       const result = await usercollection.updateOne(query, update)
       res.send(result)
     })
-    app.post('/requests', async (req, res) => {
+    app.post('/requests', verifytoken, async (req, res) => {
       const donor = req.body
       donor.status = "pending"
       donor.createdAt = new Date()
       const result = await requestcollection.insertOne(donor)
       res.send(result)
     })
-    app.get('/allrequests', async (req, res) => {
-       const query = { }
-       const { limit, skip, filter } = req.query
+    app.get('/allrequests', verifytoken,verifyadmin, async (req, res) => {
+      const query = {}
+      const { limit, skip, filter } = req.query
       if (filter && filter !== 'all') {
         query.status = filter;
       }
@@ -123,13 +170,13 @@ async function run() {
         totalCount
       })
     })
-    app.get('/requests/details/:id', async (req, res) => {
+    app.get('/requests/details/:id', verifytoken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await requestcollection.findOne(query)
       res.send(result)
     })
-    app.patch('/requests/:id', async (req, res) => {
+    app.patch('/requests/:id', verifytoken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const fields = ["recipientname", "recipientdivision", "recipientdistrict", "hospital", "fulladdress", "bloodgroup", "donationDate", "donationTime", "requestMessage", "status"];
@@ -142,17 +189,20 @@ async function run() {
       const result = await requestcollection.updateOne(query, { $set: updateData })
       res.send(result)
     })
-    app.delete('/requests/:id', async (req, res) => {
+    app.delete('/requests/:id', verifytoken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await requestcollection.deleteOne(query)
       res.send(result)
     })
 
-    app.get('/requests/:email', async (req, res) => {
+    app.get('/requests/:email', verifytoken, async (req, res) => {
       const email = req.params.email;
       const { limit, skip, filter } = req.query
       const query = { email }
+      if (email !== req.decoded_email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
       if (filter && filter !== 'all') {
         query.status = filter;
       }
